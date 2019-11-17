6e6b468f6b1216b6400b95109c3668e49b4137de
wickz80 <32722332+wickz80@users.noreply.github.com>
2019-11-17 15:43:19 -0800

    text change, background

--- src/components/article/article-title.tsx
+++ src/components/article/article-title.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from "react"
 
 export const ArticleTitle = (props: { title: string }) => (
   <h1
     style={{
       fontWeight: 550,
       textTransform: "lowercase",
-      fontFamily: "'Futura PT',-apple-system,'BlinkMacSystemFont','Segoe UI'"
+      fontFamily: `"Futura PT", -apple-system, BlinkMacSystemFont, "Segoe UI"`
     }}
   >
     {props.title}
   </h1>
-);
+)
